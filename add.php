fd9249354892b527920af6b7e283c10585a89e4d
unknown <saaluissaka>
2020-09-23 15:03:32 +0000

    formvalidation part1

--- add.php
+++ add.php
@@ -1,5 +1,39 @@
 <?php
 
+// if(isset($_GET['submit'])){
+//     echo $_GET['email'];
+//     echo $_GET['title'];
+//     echo $_GET['ingredients'];
+// }
+
+if(isset($_POST['submit'])){
+    
+    if(empty($_POST['email'])){
+        echo 'An email is required <br />';
+}else{
+    echo htmlspecialchars($_POST['email']);
+
+}
+if(empty($_POST['title'])){
+    echo 'A title is required <br />';
+}else{
+    echo htmlspecialchars($_POST['title']);
+
+
+}
+if(empty($_POST['ingredients'])){
+    echo 'At least one ingredient is required <br />';
+}else{
+    echo htmlspecialchars($_POST['ingredients']);
+    
+}
+// Validation end
+
+}
+
+
+
+
 
 ?>
 
@@ -11,7 +45,7 @@
 
 <section class='container grey-text'>
     <h4 class='center'>Add a Pizza</h4>
-    <form class='white' action='' method=''>
+    <form class='white' action='add.php' method='POST'>
         <label>Your Email:</label>
         <input type='text' name='email'>
         <label>Pizza Title:</label>
@@ -19,7 +53,7 @@
         <label>Ingredients (comma separated):</label>
         <input type='text' name='ingredients'>
         <div class='center'>
-            <input type='submit' name='submit' value='submit' class='btn brand z-depth-0'
+            <input type='submit' name='submit' value='submit' class='btn brand z-depth-0'>
         </div>
     </form>
 </section>
