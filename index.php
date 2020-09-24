c98404c230debd034a4a6fd4d647ab879e7eb2c5
unknown <saaluissaka>
2020-09-24 08:28:15 +0000

    database connection

--- index.php
+++ index.php
@@ -1,5 +1,31 @@
 <?php
 
+//connect to database      host      username:    pass:        database:  
+$conn = mysqli_connect( 'localhost', 'saalu', '123allahu', 'great_pizza');
+
+// check connection
+if(!$conn){
+    echo 'Connection error' . mysqli_connect_error();
+}
+
+// write query for all pizzas fom table(pizzas)
+$sql = 'SELECT title, ingredients, id FROM pizzas';
+
+//make query & get result
+$result = mysqli_query($conn, $sql);
+
+//fetch the resulting rows as an array
+$pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+// free results from memory
+mysqli_free_result($result);
+
+// close connection
+mysqli_close($pizzas);
+
+
+print_r($pizzas);
+
 
 ?>
 
